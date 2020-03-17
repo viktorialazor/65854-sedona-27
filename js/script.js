4d7fa83a82d2b3186a3ff3c1654a179f33c7d85b
viktoria.lazor@gmail.com <viktoria.lazor@gmail.com>
2020-03-17 23:49:55 +0200

    Fixes for animation of the form

--- js/script.js
+++ js/script.js
@@ -5,68 +5,20 @@ var dateDeparture = popup.querySelector("[name=departure-date]");
 var quantityAdult = popup.querySelector("[name=adult]");
 var quantityChildren = popup.querySelector("[name=children]");
 
-var isStorageSupport = true;
-var storageArrival = "";
-var storageDeparture = "";
-var storageAdult = "";
-var storageChildren = "";
-
-/* Нужно ли запоминать  поля, что-то   они у меня совсем не запоминаются , в лекции такое  было (но там  был пример с формой входа в личный кабинет  и запоминали только  поле Логин), а в описании задания только  анимация и чтобы  поля были не пустыми */
-try {
-	storageArrival = localStorage.getItem("dateArrival");
-} catch (err) {
-	isStorageSupport = false;
-}
-
-try {
-	storageDeparture = localStorage.getItem("dateDeparture");
-} catch (err) {
-	isStorageSupport = false;
-}
-
-try {
-	storageAdult = localStorage.getItem("quantityAdult");
-} catch (err) {
-	isStorageSupport = false;
-}
-
-try {
-	storageChildren = localStorage.getItem("quantityChildren");
-} catch (err) {
-	isStorageSupport = false;
-}
+popup.classList.add("searchHotelForm--hide");
 
 button.addEventListener("click", function(evt) {
 	evt.preventDefault();
 
-	if(popup.classList.contains("searchHotelForm--active")) {
-		popup.classList.remove("searchHotelForm--active")
+	if(popup.classList.contains("searchHotelForm--hide")) {
+		popup.classList.remove("searchHotelForm--hide");
+	}
+
+	if(popup.classList.contains("searchHotelForm--show")) {
 		popup.classList.remove("searchHotelForm--show");
 		popup.classList.remove("searchHotelForm--error");
 	} else {
-		popup.classList.add("searchHotelForm--active")
 		popup.classList.add("searchHotelForm--show");
-	}
-
-	if(storageArrival && storageDeparture && storageAdult && storageChildren) {
-		dateArrival.value = storageArrival;
-		dateDeparture.value = storageDeparture;
-		quantityAdult.value = storageAdult;
-		quantityChildren.value = storageChildren;
-	} else if(storageArrival && storageDeparture && storageAdult) {
-		dateArrival.value = storageArrival;
-		dateDeparture.value = storageDeparture;
-		quantityAdult.value = storageAdult;
-		quantityChildren.focus();
-	} else if(storageArrival && storageDeparture) {
-		dateArrival.value = storageArrival;
-		dateDeparture.value = storageDeparture;
-		quantityAdult.focus();
-	} else if(storageArrival) {
-		dateArrival.value = storageArrival;
-		dateDeparture.focus();
-	} else {
-		dateArrival.focus();
 	}
 });
 
@@ -76,19 +28,13 @@ popup.addEventListener("submit", function(evt) {
 		popup.classList.remove("searchHotelForm--error");
 		popup.offsetWidth = popup.offsetWidth;
 		popup.classList.add("searchHotelForm--error");
-	} else {
-			localStorage.setItem("arrival-date", dateArrival.value);
-			localStorage.setItem("departure-date", dateDeparture.value);
-			localStorage.setItem("adult", quantityAdult.value);
-			localStorage.setItem("children", quantityChildren.value);
-		}
+	}
 });
 
 window.addEventListener("keydown", function(evt) {
 	if(evt.keyCode === 27) {
-		if(popup.classList.contains("searchHotelForm--active")) {
+		if(popup.classList.contains("searchHotelForm--show")) {
 			evt.preventDefault();
-			popup.classList.remove("searchHotelForm--active");
 			popup.classList.remove("searchHotelForm--show");
 			popup.classList.remove("searchHotelForm--error");
 		}
